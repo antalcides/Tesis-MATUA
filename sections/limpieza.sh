fbf1c8abe492ab88855a6be25319853c3f68b63b
antalcides <antalcides@gmail.com>
2016-12-05 17:06:47 -0500

    inicio

--- sections/limpieza.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rm *.err *.nav *.snm *.out *.vrb *.py *.dvi *.ps *.eps */*.eps *.aux */*.aux *.log *.toc *.mtc* *.synctex.gz *.ptc *.idx *.maf *.bbl *.thm *.dep *.wlt *.glo *.lof *.lot *.ist &> /dev/null
